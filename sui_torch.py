65c07a65af14bfa7d44c5af83c99d98ec4515e4c
Evgeny Torbin <evgeny.torbin49@gmail.com>
2024-11-14 09:26:30 +0100

    refactor

--- sui_torch.py
+++ sui_torch.py
@@ -19,9 +19,8 @@ class Tensor:
 
     def backward(self, deltas=None):
         if deltas is not None:
-            # Check if the deltas shape matches the current tensor shape
             assert deltas.shape == self.value.shape, f'Expected gradient with shape {self.value.shape}, got {deltas.shape}'
-            self.grad = deltas  # Add the incoming gradient to the current gradient
+            self.grad = deltas
         else:
             if self.shape != tuple() and np.prod(self.shape) != 1:
                 raise ValueError(f'Can only backpropagate a scalar, got shape {self.shape}')
@@ -31,8 +30,7 @@ class Tensor:
             
             self.grad = 1.0
 
-        # Recursively call the backpropagation operation to propagate the error back
-        # Recursively call the backpropagation operation of the previous step
+        # Recursively call the backpropagation operation
         if self.back_op is not None:
             self.back_op(self.grad)
 
@@ -70,8 +68,8 @@ def add(tensor_A, tensor_B):
     result = tensor_A.value + tensor_B.value
 
     def back_op(grad_output):
-        tensor_A.grad += grad_output  # Gradient with respect to A
-        tensor_B.grad += grad_output  # Gradient with respect to B
+        tensor_A.grad += grad_output    # Gradient with respect to A
+        tensor_B.grad += grad_output    # Gradient with respect to B
         tensor_A.backward(tensor_A.grad)
         tensor_B.backward(tensor_B.grad)
 
@@ -88,7 +86,7 @@ def subtract(tensor_A, tensor_B):
 
     The gradient for B is negative as it is subtracted from A.
     """
-    result = tensor_A.value - tensor_B.value  # Element-wise subtraction.
+    result = tensor_A.value - tensor_B.value
 
     def back_op(grad_output):
         # Gradient with respect to A is the same as the grad_output.
@@ -112,13 +110,13 @@ def multiply(tensor_A, tensor_B):
 
     Each element in A is multiplied by the corresponding element in B.
     """
-    result = tensor_A.value * tensor_B.value  # Element-wise multiplication.
+    result = tensor_A.value * tensor_B.value
 
     def back_op(grad_output):
         # Gradient with respect to A is B element-wise multiplied by grad_output.
-        tensor_A.grad+= tensor_B.value * grad_output
+        tensor_A.grad += tensor_B.value * grad_output
         # Gradient with respect to B is A element-wise multiplied by grad_output.
-        tensor_B.grad+= tensor_A.value * grad_output
+        tensor_B.grad += tensor_A.value * grad_output
         tensor_A.backward(tensor_A.grad)
         tensor_B.backward(tensor_B.grad)
 
@@ -136,7 +134,7 @@ def relu(tensor):
 
     The gradient is 1 for positive elements and 0 for non-positive elements.
     """
-    result = np.maximum(0, tensor.value)  # Element-wise ReLU application.
+    result = np.maximum(0, tensor.value)
 
     def back_op(grad_output):
         # Gradient is 1 where tensor value is positive, otherwise 0.
@@ -150,14 +148,14 @@ def dot_product(tensor_A, tensor_B):
     """
     Matrix multiplication of two tensors (matrices) A and B: C = A @ B.
 
-    Formula: C = A * B (matrix multiplication)
+    Formula: C = A * B
     Gradients with respect to A and B:
     dC/dA = B^T (transpose of B)
     dC/dB = A^T (transpose of A)
 
     The gradients are the transposed versions of the input matrices.
     """
-    result = np.dot(tensor_A.value, tensor_B.value)  # Matrix multiplication (dot product).
+    result = np.dot(tensor_A.value, tensor_B.value)
 
     def back_op(grad_output):
         # Gradient with respect to A is the transpose of B multiplied by grad_output.
